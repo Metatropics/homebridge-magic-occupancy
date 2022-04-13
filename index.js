a716e67975ded53f2bd3c84c0981eaf515973244
Jason Morcos <Jason-Morcos@users.noreply.github.com>
2022-04-13 15:26:19 -0700

    Cleanup and bug fixes

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ class MagicOccupancy {
             0,
             parseInt(config.maxOccupationTimeout ?? 0, 10) ?? 0
         );
-        this.serial = config.serial.trim() ?? 'JmoMagicOccupancySwitch';
+        this.serial = (config.serial ?? 'JmoMagicOccupancySwitch').trim();
         this.persistBetweenReboots = config.persistBetweenReboots != false;
         this.startOnReboot = config.startOnReboot || false;
         this.triggerSwitchToggleTimeout = 1000;
