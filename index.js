4d00900ed4d4d2eb4ba7d604ad9631e46aae91ef
Clay Oster <iamclayoster@gmail.com>
2023-01-09 21:36:05 -0600

    Moving existing logging to debug mode.

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ class MagicOccupancy {
         this.occupancyService
             .getCharacteristic(Characteristic.TimeoutDelay)
             .on('change', event => {
-                this.log('Setting stay occupied delay to:', event.newValue)
+                this.log.debug('Setting stay occupied delay to:', event.newValue)
                 this.stayOccupiedDelay = event.newValue
             });
 
@@ -177,7 +177,7 @@ class MagicOccupancy {
             .getCharacteristic(Characteristic.TimeRemaining)
             .on('change', event => {
                 if (event.newValue === 0 && event.oldValue > 0) {
-                    this.log('Cancel timer and set occupancy to "NotDetected"')
+                    this.log.debug('Cancel timer and set occupancy to "NotDetected"')
                     this.setOccupancyNotDetected()
                 }
             });
@@ -303,7 +303,7 @@ class MagicOccupancy {
 
         //Handle start on reboot
         if (this.startOnReboot) {
-            this.log(`startOnReboot==true - setting to active`)
+            this.log.debug(`startOnReboot==true - setting to active`)
             //Run the set after homebridge should have booted to ensure events fire
             setTimeout(
                 function () {
@@ -376,7 +376,7 @@ class MagicOccupancy {
         this.stop();
 
         this._timer_started = new Date().getTime();
-        this.log('Timer startUnoccupiedDelayed:', timeRemaining);
+        this.log.debug('Timer startUnoccupiedDelayed:', timeRemaining);
 
         this._timer = setTimeout(
             this.setOccupancyNotDetected.bind(this),
@@ -414,7 +414,7 @@ class MagicOccupancy {
      */
     stop () {
         if (this._timer) {
-            this.log('Delay timer stopped');
+            this.log.debug('Delay timer stopped');
             clearTimeout(this._timer);
             clearInterval(this._interval);
             this._timer = null;
@@ -510,7 +510,7 @@ class MagicOccupancy {
 
         //Clear max timeout
         if (this._max_occupation_timer != null) {
-            this.log('Max occupation timer stopped');
+            this.log.debug('Max occupation timer stopped');
             clearTimeout(this._max_occupation_timer);
             this._max_occupation_timer = null;
         }
@@ -594,7 +594,7 @@ class MagicOccupancy {
                     this.setOccupancyDetected();
                 }
 
-                this.log(
+                this.log.debug(
                     `checkOccupancy result: true. Previous state: ${previousModeState}, current state: ${this.modeState}`
                 );
             }
@@ -606,7 +606,7 @@ class MagicOccupancy {
                     this.startUnoccupiedDelay();
                 }
 
-                this.log(
+                this.log.debug(
                     `checkOccupancy result: false. Previous state: ${previousModeState}, current state: ${this.modeState}`
                 );
             }
@@ -623,7 +623,7 @@ class MagicOccupancy {
                 .getValue(
                     function (err, value) {
                         if (err) {
-                            this.log(
+                            this.log.debug(
                                 `ERROR GETTING VALUE Characteristic.KeepingOccupancyTriggered ${err}`
                             )
                             value = false
@@ -641,7 +641,7 @@ class MagicOccupancy {
         ) {
             this.startUnoccupiedDelay();
 
-            this.log(
+            this.log.debug(
                 `checkOccupancy result: false (0 switches). Previous occupied state: ${previousModeState}, current state: ${this.modeState}`
             );
         }
